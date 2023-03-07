51a54c67846936254d5aa635a0134c5d7c666c65
coder <79178809+c0deblack@users.noreply.github.com>
2023-03-06 23:10:03 -0500

    Completed ItemDao and Item Table

--- src/main/resources/Tables.sql
+++ src/main/resources/Tables.sql
@@ -1,3 +1,16 @@
 --h2 is typically used to setup a test database, not a prod database.
 --first, drop your tables (to reset your database for testing)
 --then create your tables
+
+
+-- Item Table
+
+DROP TABLE IF EXISTS item;
+
+CREATE TABLE IF NOT EXISTS item (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    price DECIMAL(10,2),
+    description VARCHAR(1024),
+    store_id INTEGER NOT NULL
+);
